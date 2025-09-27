bb5a750d23ca1c81229af0e58dc7cf1cc8739cef
KadirEmreAVCI <kadiremreavc@gmail.com>
2025-09-27 14:16:50 +0300

    Constructor of class UpcomingMatch has taken only Match parameter and root Tournament and Organization has been found using DatabaseController.

--- src/app/HomePage.cpp
+++ src/app/HomePage.cpp
@@ -37,8 +37,7 @@ void HomePage::UpdateUpcomingMatches()
 		});
 	for (const auto& m : vecUpcomingMatches)
 	{
-		const Tournament rootTournament = DatabaseController::instance().FindRootTournament(m);
-		InsertUpcomingMatch(new UpcomingMatch(this, (rootTournament.GetOrgPictureAddr() != "") ? rootTournament.GetOrgPictureAddr() : "default_org.png", rootTournament, m));
+		InsertUpcomingMatch(new UpcomingMatch(m, this));
 	}
 	FillWithNoUpcomingMatches();
 	ui.listWidget_UpcomingMatches->setFixedSize(ui.listWidget_UpcomingMatches->sizeHintForColumn(0) + 15, g_uiUpcomingMatchHeight * g_uiMaxUpcomingMatch + 10);
