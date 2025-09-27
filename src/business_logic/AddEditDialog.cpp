6cba892d184244e14089776aff56eae603e95ab2
KadirEmreAVCI <kadiremreavc@gmail.com>
2025-09-27 10:02:16 +0300

    The commented codes have been deleted from class AddEditDialog.

--- src/business_logic/AddEditDialog.cpp
+++ src/business_logic/AddEditDialog.cpp
@@ -40,36 +40,6 @@ void AddEditDialog<T>::SetDialogMode(DialogMode mode)
 {
 	m_DialogMode = mode;
 }
-// template <typename T>
-// bool AddEditDialog<T>::SaveImage()const
-// {
-// 	if (QFile::exists(m_sFullDestDir))
-// 	{
-// 		QFile::remove(m_sFullDestDir);
-// 	}
-// 	return QFile::copy(m_sFullSourceDir, m_sFullDestDir);
-// } 
-// template <typename T>
-// void AddEditDialog<T>::ClearImage()
-// {
-// 	m_sFullSourceDir = "";
-// 	m_sFullDestDir = "";
-// 	m_sImageFileName = "";
-// }
-// template <typename T>
-// void AddEditDialog<T>::OnBrowseButtonClicked(const QString& sFullSourceDir, const QString& sRootDestDir)
-// {
-// 	m_sFullSourceDir = sFullSourceDir;
-// 	if (m_sImageFileName = QFileInfo(m_sFullSourceDir).fileName(); m_sImageFileName != "")
-// 	{
-// 		QDir destDir(sRootDestDir);
-// 		if (!destDir.exists())
-// 		{
-// 			destDir.mkpath(sRootDestDir);
-// 		}
-// 		m_sFullDestDir = destDir.filePath(m_sImageFileName);
-// 	}
-// }
 template class AddEditDialog<Organization>;
 template class AddEditDialog<Profile>;
 template class AddEditDialog<Tournament>;
